fe488a93d96d115d5267174e5368d5007b718cb9
Robert Turner <robturneris@gmail.com>
2022-03-04 22:56:45 -0500

    Added Head to post

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <Title>First Post</Title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
